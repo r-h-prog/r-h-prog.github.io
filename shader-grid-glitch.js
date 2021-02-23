f12824ce188ebeeb37d6c705df9abd284f86af4e
r-h-prog <54469188+r-h-prog@users.noreply.github.com>
2021-02-23 11:05:39 +0900

    3

--- shader-grid-glitch.js
+++ shader-grid-glitch.js
@@ -1,55 +1,40 @@
 /* global AFRAME, THREE */
+//https://glitch.com/edit/#!/aframe-simpler-shader?path=index.html:18:17
+// shader-grid-glitch.js
 
-const glsl = require('glslify');
-const vertexShader = glsl.file('../shaders/vertex.glsl');
-const fragmentShader = glsl.file('../shaders/fragment.glsl');
-
-AFRAME.registerComponent('material-grid-glitch', {
-  schema: {color: {type: 'color'}},
-
-  /**
-   * Creates a new THREE.ShaderMaterial using the two shaders defined
-   * in vertex.glsl and fragment.glsl.
-   */
-  init: function () {
-    const data = this.data;
-  
-    this.material  = new THREE.ShaderMaterial({
-      uniforms: {
-        time: { value: 0.0 },
-        color: { value: new THREE.Color(data.color) }
-      },
-      vertexShader,
-      fragmentShader
-    });
-
-    this.applyToMesh();
-    this.el.addEventListener('model-loaded', () => this.applyToMesh());
+AFRAME.registerShader('grid-glitch', {
+  schema: {
+    color: {type: 'color', is: 'uniform'},
+    timeMsec: {type: 'time', is: 'uniform'}
   },
 
+  vertexShader: `
+varying vec2 vUv;
 
-  /**
-   * Update the ShaderMaterial when component data changes.
-   */
-  update: function () {
-    this.material.uniforms.color.value.set(this.data.color);
-  },
-    
-  /**
-   * Apply the material to the current entity.
-   */
-  applyToMesh: function() {
-    const mesh = this.el.getObject3D('mesh');
-    if (mesh) {
-      mesh.material = this.material;
-    }
-  },
+void main() {
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`,
+  fragmentShader: `
+varying vec2 vUv;
+uniform vec3 color;
+uniform float timeMsec; // A-Frame time in milliseconds.
 
-  /**
-   * On each frame, update the 'time' uniform in the shaders.
-   */
-  tick: function (t) {
-    this.material.uniforms.time.value = t / 1000;
-  }
-  
-})
+void main() {
+  float time = timeMsec / 2000.0; // Convert from A-Frame milliseconds to typical time in seconds.
+  // Use sin(time), which curves between 0 and 1 over time,
+  // to determine the mix of two colors:
+  //    (a) Dynamic color where 'R' and 'B' channels come
+  //        from a modulus of the UV coordinates.
+  //    (b) Base color.
+  // 
+  // The color itself is a vec4 containing RGBA values 0-1.
+  gl_FragColor = mix(
+    vec4(mod(vUv , 0.05) * 20.0, 1.0, 1.0),
+    vec4(color, 1.0),
+    sin(time)
+  );
+}
+`
+});
